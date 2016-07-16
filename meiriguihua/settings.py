5e87bb5d08302bfc59c58c1b957b282959a6fdcf
zhengxiaowai <h1x2y3awalm@gmail>
2016-07-16 11:31:08 +0800

    to mysql

--- meiriguihua/settings.py
+++ meiriguihua/settings.py
@@ -77,14 +77,14 @@ WSGI_APPLICATION = 'meiriguihua.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-       # 'ENGINE': 'django.db.backends.mysql',
-       # 'NAME': os.environ.get('DATABASE_NAME', 'meiriguihua'),
-       # 'USER': os.environ.get('DATABASE_USER', 'root'),
-       # 'PASSWORD': os.environ.get('DATABASE_PASSWORD', 'mysql'),
-       # 'HOST': os.environ.get('DATABASE_HOST', '127.0.0.1'),
-       # 'PORT': os.environ.get('DATABASE_PORT', '3306'),
+       # 'ENGINE': 'django.db.backends.sqlite3',
+       # 'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': os.environ.get('DATABASE_NAME', 'meiriguihua'),
+        'USER': os.environ.get('DATABASE_USER', 'root'),
+        'PASSWORD': os.environ.get('DATABASE_PASSWORD', 'mysql'),
+        'HOST': os.environ.get('DATABASE_HOST', '127.0.0.1'),
+        'PORT': os.environ.get('DATABASE_PORT', '3306'),
     }
 }
 
